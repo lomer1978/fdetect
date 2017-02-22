4ccbebc14ba2382a2b84d033041fb051bd549420
Omer Levy <omer.levy@intel.com>
2017-02-22 23:12:02 +0200

    added flip, bugfix

--- gen_datasets.py
+++ gen_datasets.py
@@ -4,37 +4,53 @@ from PIL import Image
 import numpy as np
 import sys
 import os
-
-from six.moves import xrange  # pylint: disable=redefined-builtin
-from six.moves import urllib
-
-#from resnet_train import train
-#from resnet import inference_small
+import random
+import string
 
 IMAGE_SIZE=224
 def normalize_images(file1, file2):
     dstsize = [IMAGE_SIZE, IMAGE_SIZE]
     img1 = Image.open(file1)
+    sz1 = img1.size
     img1=img1.resize(dstsize, PIL.Image.ANTIALIAS)
     img2 = Image.open(file2)
+    sz2 = img2.size
     img2=img2.resize(dstsize, PIL.Image.ANTIALIAS)
     data1 = np.ascontiguousarray(np.fromstring(img1.tobytes(), dtype=np.uint8).reshape(img1.size+(3,))[:,:,1], dtype=np.uint8)
     data2 = np.ascontiguousarray(np.fromstring(img2.tobytes(), dtype=np.uint8).reshape(img2.size+(3,))[:,:,1], dtype=np.uint8)
-    return np.stack((data1, data2),2), dstsize, dstsize
+    return np.stack((data1, data2),2), sz1, sz2
 
+def rnd_flip(img, lbl):
+    newlbl = lbl
+    if random.randint(1,2) == 1:
+        newimg = np.ascontiguousarray(np.fliplr(img))
+        for i in range(0, len(lbl), 4):
+            if lbl[i] != 0 and lbl[i+1] != 0: #valid entry
+                newlbl[i] = 1 - newlbl[i]
+    else:
+        newimg = img
+                
+    return (newimg, newlbl)
+    
 def normalize_label(file, size):
-    s=""
+    s=[]
     fd=open(file, "r")
     for i in range(4):
         list=fd.readline().split();
-        if len(list) != 3 or list[2] < 3:
-            s+= "0 0 0 0 "
+        if len(list) != 3 or list[2] < 3: #ignore small radius markings
+            s+= [0, 0, 0, 0]
         else:
-            s+= str(float(list[0])/float(size[0])) + " " + str(float(list[1])/float(size[1])) + " " + str(float(list[2])/float(size[0])) + " " + str(float(list[2])/float(size[1])) + " "
+            s+= [float(list[0])/float(size[0]), float(list[1])/float(size[1]), float(list[2])/float(size[0]), float(list[2])/float(size[1])]
     fd.close()
     return s
 
+AUG_PER_SAMPLE=3
+
+def flist2str(flist):
+    return string.join([str(f) for f in flist])
+        
 def maybe_convert_dataset(srcpath, dstpath):
+    random.seed()
     if not os.path.exists(dstpath+"converted"):
         filelist=glob.glob(srcpath+"/*AP_Image.jpg")
         count=0
@@ -55,9 +71,20 @@ def maybe_convert_dataset(srcpath, dstpath):
             fd.close()
 
             fd = open(f.replace(srcpath, dstpath).replace("AP_Image.jpg", "Lbl.txt"), "wb")
-            fd.write(newaplbl + newltlbl)
+            fd.write(flist2str(newaplbl + newltlbl))
             fd.close()
 
+            for i in range(AUG_PER_SAMPLE):
+                (augimage, auglabel) = rnd_flip(newimage, newaplbl + newltlbl)
+                fd = open(f.replace(srcpath, dstpath).replace("AP_Image.jpg", "Image-"+str(i)+".dat"), "wb")
+                fd.write(augimage)
+                fd.close()
+
+                fd = open(f.replace(srcpath, dstpath).replace("AP_Image.jpg", "Lbl-"+str(i)+".txt"), "wb")
+                fd.write(flist2str(auglabel))
+                fd.close()
+
+                
             sys.stdout.write('\r>> converting %s %.1f%%' %
                              (srcpath, float(count) /
                               float(total_size) * 100.0))
