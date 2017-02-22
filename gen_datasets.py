0303f945eca6238c61cb34cddffe9e6ce8e04abc
Omer Levy <omer.levy@intel.com>
2017-02-22 22:10:51 +0200

    added datview utility

--- gen_datasets.py
+++ gen_datasets.py
@@ -8,28 +8,29 @@ import os
 from six.moves import xrange  # pylint: disable=redefined-builtin
 from six.moves import urllib
 
-from resnet_train import train
-from resnet import inference_small
+#from resnet_train import train
+#from resnet import inference_small
 
+IMAGE_SIZE=224
 def normalize_images(file1, file2):
+    dstsize = [IMAGE_SIZE, IMAGE_SIZE]
     img1 = Image.open(file1)
+    img1=img1.resize(dstsize, PIL.Image.ANTIALIAS)
     img2 = Image.open(file2)
-    img2=img2.resize(img1.size, PIL.Image.ANTIALIAS)
+    img2=img2.resize(dstsize, PIL.Image.ANTIALIAS)
     data1 = np.ascontiguousarray(np.fromstring(img1.tobytes(), dtype=np.uint8).reshape(img1.size+(3,))[:,:,1], dtype=np.uint8)
-    data2 = np.ascontiguousarray(np.fromstring(img2.tobytes(), dtype=np.uint8).reshape(img1.size+(3,))[:,:,1], dtype=np.uint8)
-    return np.stack((data1, data2),2), img1.size, img2.size
+    data2 = np.ascontiguousarray(np.fromstring(img2.tobytes(), dtype=np.uint8).reshape(img2.size+(3,))[:,:,1], dtype=np.uint8)
+    return np.stack((data1, data2),2), dstsize, dstsize
 
 def normalize_label(file, size):
     s=""
     fd=open(file, "r")
-    while True:
+    for i in range(4):
         list=fd.readline().split();
-        if len(list) != 3:
-            break
-        s+= str(float(list[0])/float(size[0]))+" "
-        s+= str(float(list[1])/float(size[1]))+" "
-        s+= str(float(list[2])/float(size[0]))+" "
-        s+= str(float(list[2])/float(size[1]))+"\n"
+        if len(list) != 3 or list[2] < 3:
+            s+= "0 0 0 0 "
+        else:
+            s+= str(float(list[0])/float(size[0])) + " " + str(float(list[1])/float(size[1])) + " " + str(float(list[2])/float(size[0])) + " " + str(float(list[2])/float(size[1])) + " "
     fd.close()
     return s
 
@@ -53,12 +54,8 @@ def maybe_convert_dataset(srcpath, dstpath):
             fd.write(newimage)
             fd.close()
 
-            fd = open(ltlfile.replace(srcpath, dstpath), "wb")
-            fd.write(newltlbl)
-            fd.close()
-
-            fd = open(aplfile.replace(srcpath, dstpath), "wb")
-            fd.write(newaplbl)
+            fd = open(f.replace(srcpath, dstpath).replace("AP_Image.jpg", "Lbl.txt"), "wb")
+            fd.write(newaplbl + newltlbl)
             fd.close()
 
             sys.stdout.write('\r>> converting %s %.1f%%' %
